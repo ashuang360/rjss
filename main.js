2aa29071a15e54e029d910ef98547d1300aa2494
ashuang360 <91492276+ashuang360@users.noreply.github.com>
2022-11-20 13:22:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -74,11 +74,20 @@ function ssGo() {
         "blog.ruancang.net",
         "ghxi.com",
         "www.luochenzhimu.com",
+        "baideye.com",
         "www.macyy.cn",
         "www.lxapk.com",
+        "www.iplaysoft.com",
         "www.torrentmac.net",
         "www.yxssp.com",
-        "fy6b.com"
+        "fy6b.com",
+        "www.sdifen.com",
+        "macpedia.xyz",
+        "softasm.com",
+        "igetintopc.com",
+        "www.sadeempc.com",
+        "haxnode.net",
+        "www.crackingcity.com"
     ];
 
     website.forEach(function (element) {
